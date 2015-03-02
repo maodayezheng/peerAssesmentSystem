b3bbe8cdfa031c63214803e728e4225253dfe0f6
jobarry <jobarry@tcd.ie>
2015-03-02 21:59:52 +0000

    code instructions

--- WebContent/PHP/loginVerify.php
+++ WebContent/PHP/loginVerify.php
@@ -4,6 +4,8 @@ require_once ('DBConnection.php');
 
  	$username = $_POST ['username'];
  	$password = $_POST ['password'];
+ 	
+ 	//CONFIRM USERNAME + PASSWORD MATCH + CONFIRM LOGIN
 
 $query="SELECT * from account where userName = '$username' and password = '$password'";
 $result = mysqli_query ($conn,$query);
@@ -14,36 +16,5 @@ if(mysqli_num_rows($result)==1){
 } else {
 	echo "incorrect username / password";
 }
-
-// if (user_exists('john') === true){
-// 	echo 'user exists';
-// }
-
-// if(empty($_POST)===false){
-// 	$username = $_POST ['username'];
-// 	$password = $_POST ['password'];
-	
-// 	if(empty($username)===true || empy($password)===true){
-// 		$errors[] = 'you need to enter a username and password';
-// 	} else if (user_exists($username) === false) {
-// 		$errors[] = "We cant find that username in db, are you sure you registered?";
-// 	}
-// }
-
-// echo "HERE";
-
-
-
-// if ($username && $password) {
-	
-// 	mysqli_select_db ( $conn, "peersystem" ) or die ( "couldn't find db" );
-	
-// 	$query = mysql_query("SELECT * FROM account WHERE userName='$username'");
-// 	$numrows = mysql_num_rows($query);
-// 	echo $numrows;
-	
-// } else {
-// 	echo "Please enter username and password";
-// }
 
 ?>
