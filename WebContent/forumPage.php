2ea32fe9986d9b99c1257a518a94ec2e262bceae
jobarry <jobarry@tcd.ie>
2015-03-03 01:58:53 +0000

    forum post modal

--- WebContent/forumPage.php
+++ WebContent/forumPage.php
@@ -1,35 +1,74 @@
 	<?php
-			include("header.php"); 
-			include("navbar.php")
-			?>
-			<div class="container" style="margin-top: 60px">
-  <div class="panel panel-primary">
-    <div class="panel-heading">Forum Page</div>
-    <div class="panel-body">
-      <table class="table table-striped table-hover">
-        <thead>
-          <tr>
-            <th colspan="2">Title</th>
-            <th>Content</th>
-            <th>Date</th>
+	include ("header.php");
+	include ("navbar.php")?>
+<div class="container" style="margin-top: 60px">
+	<div class="panel panel-primary">
+		<div class="panel-heading">Forum Page</div>
+		<div class="panel-body">
+			<table class="table table-striped table-hover">
+				<thead>
+					<tr>
+						<th colspan="2">Title</th>
+						<th>Content</th>
+						<th>Date</th>
 
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td colspan="2">Stuck on Q3</td>
-            <td>MIPS is hard</td>
-            <td>23/1/15</td>
-          </tr>
-          <tr>           
-            <td colspan="2">Great lecture from Dean</td>
-            <td>Dean is on top of his game</td>
-            <td>23/1/15</td>
-          </tr>
-        </tbody>
-      </table>
-      <a href="#" class="btn btn-primary btn-bg pull-right">Post</a>
-    </div>
-  </div>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td colspan="2">Stuck on Q3</td>
+						<td>MIPS is hard</td>
+						<td>23/1/15</td>
+					</tr>
+					<tr>
+						<td colspan="2">Great lecture from Dean</td>
+						<td>Dean is on top of his game</td>
+						<td>23/1/15</td>
+					</tr>
+				</tbody>
+			</table>
+			<a href="#postForum" data-toggle="modal" data-target="#postForum"
+				class="btn btn-primary btn-bg pull-right">Post</a>
+
+			<div class="modal fade" id="postForum" tabindex="-1" role="dialog"
+				aria-labelledby="postForum" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h4 class="modal-title" id="myModalLabel">Post to Forum</h4>
+						</div>
+						<div class="modal-body">
+							<div class="panel-body">
+
+								<div>
+									<h4>
+										<b>Title:</b>
+									</h4>
+									<textarea name="titleForum" id="titleForum" max="50" rows="1"
+										cols="50" placeholder="Title:"></textarea>
+									<br>
+								</div>
+
+								<div>
+									<h4>
+										<b>Content:</b>
+									</h4>
+									<textarea name="contentForum" id="contentForum" max="10000"
+										rows="5" cols="50" placeholder="Write your post here"></textarea>
+									<br>
+								</div>
+							</div>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default"
+								data-dismiss="modal">Close</button>
+							<button type="button" class="btn btn-success"
+								data-dismiss="modal" onclick="submitReport()">Submit</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 </html>
--- WebContent/header.php
+++ WebContent/header.php
@@ -5,7 +5,15 @@
 <title>Peer Review</title>
 <!-- makes sure it displays the mobile version on mobile and desktop version on desktop-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="libs/jquery/jquery-2.1.1.min.js"></script>
+<script src="libs/jquery/jquery-ui-1.11.2.min.js"></script>
+<script src="libs/bootstrap/bootstrap.min.js"></script>
+
+<link href= "libs/jquery/jquery-ui-1.11.2.min.css" rel="stylesheet" type="text/css">
 <link href="libs/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css">
 <link href="CSS/standard.css" rel="stylesheet" type="text/css">
+
 </head>
 <body>
