b7a26e23c5aeea7516bb407173802ba4546bb856
vab2048 <vab2048@gmail.com>
2015-03-10 13:17:45 +0000

    Added link to resultsAndRanking php page

--- WebContent/JavaScript/Navigator.js
+++ WebContent/JavaScript/Navigator.js
@@ -10,10 +10,10 @@ Navigator.prototype.navigate = function(origin)
 
     switch(origin)
     {
-        case "submit_group_report":             nextPage = "submitReport.php"; break;
-        case "view_results_and_ranking":        alert("Set This hyperlink in navigator.js"); break;
-        case "visit_the_forum":                 nextPage = "forumPage.php"; break;
-        case "conduct_a_peerwise_assessment":   alert("Set This hyperlink in navigator.js"); break;
+        case "submit_group_report":             nextPage = "submitReport.php";                  break;
+        case "view_results_and_ranking":        nextPage = "resultsAndRanking.php";             break;
+        case "visit_the_forum":                 nextPage = "forumPage.php";                     break;
+        case "conduct_a_peerwise_assessment":   alert("Set This hyperlink in navigator.js");    break;
 
         default: break;
     }
