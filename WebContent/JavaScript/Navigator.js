99c47c398300106cb148431113207ce097fe8d5f
vab2048 <vab2048@gmail.com>
2015-03-10 03:34:34 +0000

    Added link to forum in navigator

--- WebContent/JavaScript/Navigator.js
+++ WebContent/JavaScript/Navigator.js
@@ -12,7 +12,7 @@ Navigator.prototype.navigate = function(origin)
     {
         case "submit_group_report":             nextPage = "submitReport.php"; break;
         case "view_results_and_ranking":        alert("Set This hyperlink in navigator.js"); break;
-        case "visit_the_forum":                 alert("Set This hyperlink in navigator.js"); break;
+        case "visit_the_forum":                 nextPage = "forumPage.php"; break;
         case "conduct_a_peerwise_assessment":   alert("Set This hyperlink in navigator.js"); break;
 
         default: break;
