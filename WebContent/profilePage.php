7454ee925a8b14ec16796ef9bc9ce30f53a24061
jobarry <jobarry@tcd.ie>
2015-03-12 15:06:36 +0000

    basic css changes + profile page (not yet dynamic)

---
+++ WebContent/profilePage.php
@@ -0,0 +1,69 @@
+<?php
+include ("header.php");
+include ("navbar.php");
+?>
+<div style="padding-top:20px; padding-left:700px" class="col-md-9">
+<div class="container-fluid well span6">
+	<div class="row-fluid">
+        <div class="span2" >
+		    <img align="middle" class= "img-circular" src="http://sandman.com.au/wp-content/uploads/2013/02/placeholder-photo.png" height="150" width="150">
+        </div><br>
+        
+                  <table class="table table-user-information">
+                    <tbody>
+                      <tr>
+                        <td>Name:</td>
+                        <td>John Smith</td>
+                      </tr>
+                       <tr>
+                        <td>Username:</td>
+                        <td>JSmith</td>
+                      </tr>
+                      <tr>
+                        <td>User Status:</td>
+                        <td>Commercial</td>
+                      </tr>
+                      <tr>
+                        <td>Date of Birth</td>
+                        <td>01/24/1988</td>
+                      </tr>
+                   
+                         <tr>
+                             <tr>
+                        <td>Gender</td>
+                        <td>Male</td>
+                      </tr>
+                        <tr>
+                        <td>Home Address</td>
+                        <td>45 Aston Way, London, UK</td>
+                      </tr>
+                      <tr>
+                        <td>Email</td>
+                        <td><a href="mailto:info@support.com">info@tool.com</a></td>
+                      </tr>
+                        <td>Phone Number</td>
+                        <td>123-4567-890(Landline)<br><br>555-4567-890(Mobile)
+                        </td>
+                           
+                      </tr>
+                     
+                    </tbody>
+                  </table>
+        
+        <div class="span2">
+            <div class="btn-group">
+                <a class="btn dropdown-toggle btn-info" data-toggle="dropdown" href="#">
+                    Action 
+                    <span class="icon-cog icon-white"></span><span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="#"><span class="icon-wrench"></span> Edit Info</a></li>
+                    <li><a href="#"><span class="icon-trash"></span> Change User Status</a></li>
+                </ul>
+            </div>
+        </div>
+</div>
+</div>
+</div>
+</body>
+</html>
