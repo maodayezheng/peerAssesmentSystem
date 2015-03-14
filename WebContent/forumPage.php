ecb1f677034e564fe5959e060f363d30ccf026dc
vab2048 <vab2048@gmail.com>
2015-03-14 20:12:41 +0000

    Forum has CSS and HTML elements completed for advanced search, now only need to finalise the logic for the searches

--- WebContent/forumPage.php
+++ WebContent/forumPage.php
@@ -12,7 +12,7 @@
             <h4 class="panel-title">
                 <form action="forumPage.php" method="post">
                     <div class="input-group">
-                        <input type="text" name="searchTerms" class="form-control" placeholder="Search the Forum">
+                        <input type="text" name="searchQuery" class="form-control" placeholder="Search the Forum">
                         <span class="input-group-btn">
                             <a data-toggle="collapse" data-target="#advancedSearch" href="#advancedSearch"
                                 <button type="button" class="btn btn-default">Advanced Search </button>
@@ -26,12 +26,14 @@
                     <!-- Clicking the advanced search button will open this collapsed div -->
                     <div id="advancedSearch" class="panel-collapse collapse">
                         <div class="panel-body">
-                            <p> Enter advanced search information here. </p>
-                            <select name="filters">
+                            <p> Choose a filter for the search:
+                            <select name="filter">
                                 <option value="WholeForum">Whole Forum</option>
                                 <option value="ThreadTitlesOnly">Thread Titles Only</option>
                                 <option value="PostsOnly">Posts Only</option>
+                                <option value="GroupMembersOnly">Group Members Only</option>
                             </select>
+                            </p>
                         </div>
                     </div>
 
@@ -40,25 +42,51 @@
         </div>
     </div>
 
-
-
-
-<!-- Process the search query if it is set and not equal to the empty string. -->
+<!-- Forum Showing All Entries for the group. -->
 <?php
-    if (isset($_POST["searchTerms"]) && (isset($_POST["searchTerms"]) != ""))
-    {
-        echo "search term is set";
-    } else
-    {
-        // echo "search term is not set";
-    }
+    include("pageSnippets/forum/forum.php");
 ?>
 
 
-<!-- Forum Showing All Entries for the group. Shown when no search terms are presented to the search bar.
----- When a search is made, this is removed from the DOM. -->
+
+<!-- Logic to process the search query and if it is set and not equal to the empty string. -->
 <?php
-    include("pageSnippets/forum/forum.php");
+if (isset($_POST["searchQuery"]) && (isset($_POST["searchQuery"]) != ""))
+{
+    // Perform a regular expression search and replace of the parameter passed in by the user. for any characters in the first argument
+    // This is to allow only certain characters sent to the database. In this case anything which is not a letter, not a number,
+    // not a ' ' space, not a '?', and not a '!' will be replaced with the empty string ''.
+    // e.g. '; INSERT INTO ... (...) VALUES (...)' will not allow the ';', '(', and ')' characters.
+    $searchQuery = preg_replace('#[^a-z 0-9?!]#i', '', $_POST["searchQuery"]);
+
+    if($_POST['filter'] === "WholeForum")
+    {
+        echo "in WholeForum";
+    } else if($_POST['filter'] === "ThreadTitlesOnly")
+    {
+        echo "in threadTitles Only";
+    } else if($_POST['filter'] === "PostsOnly")
+    {
+        echo "in Posts Only";
+    } else if($_POST['filter'] === "GroupMembersOnly")
+    {
+        echo "in group members only";
+    }
+
+    // Connect to the database, run the query and dynamically generate the data from the search results.
+    $count = 1; //num rows
+    if($count > 0)
+    {
+        // Logic to generate the forum containing the search results.
+    } else
+    {
+
+    }
+
+} else
+{
+    echo "search term is not set";
+}
 ?>
 
 
