53b2ccec7d884bb506654f8291f5ac197f89d608
vab2048 <vab2048@gmail.com>
2015-03-14 18:29:30 +0000

    Created new page forum.php which has the job of dynamically generating a forum based on what search term is passed (or list everything)

--- WebContent/forumPage.php
+++ WebContent/forumPage.php
@@ -1,47 +1,53 @@
 <?php
 	require_once ("PHP/init.php");
     include ("header.php");
-	include ("navbar.php")
+	include ("navbar.php");
+
+
 ?>
 
 
-<div class="container" style="margin-top: 60px">
-    <div class="panel panel-primary">
-		<div class="panel-heading">
-            <table>
-                <tr>
-                    <th style="font-size: 20px;" >
-                        <div style="float:left; width=50%;">
-                        <?php
-                            echo 'Welcome '. htmlspecialchars($_SESSION["userName"]).' to Group '.
-                                htmlspecialchars($_SESSION["peergroup"]).'\'s Discussion Forum';
-                        ?>
-                        </div>
-                    </th>
-                </tr>
-            </table>
+
+<!-- Search Box Settings -->
+    <div class="panel panel-default" id="panel2">
+        <div class="panel-heading">
+            <h4 class="panel-title">
+                <form action="forumPage.php" method="post">
+                    <div class="input-group">
+                        <input type="text" name="searchTerms" class="form-control" placeholder="Search the Forum">
+
+                        <span class="input-group-btn">
+                                <button type="submit" class="btn btn-default">Advanced Search </button>
+                           </span>
+
+                            <span class="input-group-btn">
+                                <button type="submit" class="btn btn-default">Search</button>
+                           </span>
+                    </div>
+                </form>
+            </h4>
         </div>
+    </div>
 
-            <!-- Table containing each thread for the group. -->
-            <table class="table table-striped table-hover">
-				<thead>
-					<tr>
-						<th style="text-align: center; font-size: 20px; width: 80%" >
-                            Forum Threads</th>
-                        <th style="text-align: center; font-size: 20px; width: 20%">
+<!-- Process the search query if it is set and not equal to the empty string. -->
+<?php
+    if (isset($_POST["searchTerms"]) && (isset($_POST["searchTerms"]) != ""))
+    {
+        echo "search term is set";
+    } else
+    {
+        echo "search term is not set";
+    }
+?>
 
-                            <?php include("pageSnippets/forum/createNewThreadButton.html"); ?>
 
-                        </th>
-					</tr>
-				</thead>
-				<tbody>
+<!-- Forum Showing All Entries for the group. Shown when no search terms are presented to the search bar.
+---- When a search is made, this is removed from the DOM. -->
+<?php
+    include("pageSnippets/forum/forum.php");
+?>
 
-                    <?php include ("pageSnippets/forum/ForumTable.php")?>
 
-                </tbody>
-			</table>
-		</div>
-	</div>
+
 </div>
 </html>
