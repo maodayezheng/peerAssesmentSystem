993fc0a055da9f5fb5d5f47c0ba48334417dd3c8
vab2048 <vab2048@gmail.com>
2015-03-16 02:22:47 +0000

    Altered register form to be more user friendly (drop down list for gender instead of raw input).

--- WebContent/PHP/newAccount.php
+++ WebContent/PHP/newAccount.php
@@ -5,17 +5,19 @@ if (isset ( $_POST ['submit'] )) {
 	
 	$conn = new mysqli ( $servername, $username, $password, $dbname, $port );
 	
-	$userName = $_POST ['username'];
-	$fname = $_POST ['fname'];
-	$lname = $_POST ['lname'];
-	$sex = $_POST ['sex'];
-	$password = $_POST ['password'];
-	$accounttype = $_POST ['accounttype'];
-	$sql = "INSERT INTO account (userName,fName,lName,password,sex,accountType,id) VALUES ('$userName','$fname','$lname','$password','$sex','$accounttype',NULL)";
+	$userName       = $_POST ['username'];
+	$fname          = $_POST ['fname'];
+	$lname          = $_POST ['lname'];
+	$sex            = $_POST ['sex'];
+	$password       = $_POST ['password'];
+
+
+	$sql = "INSERT INTO account (userName,fName,lName,password,sex,accountType,id)
+            VALUES ('$userName','$fname','$lname','$password','$sex','$accounttype',NULL)";
 	
 	if ($conn->query ( $sql ) === true) {
 		// output data of each row
-		echo "Selection complete";
+		echo "Registration successful";
 		header ( 'location: ../login.php' );
 	} else {
 		echo "Error:" . $sql . "<br>" . $conn->error;
