8b380b395992986bbf7a214f85d7918b0734b281
vab2048 <vab2048@gmail.com>
2015-03-16 05:37:11 +0000

    Finalised bare bones HTML for the search bar

--- WebContent/pageSnippets/forum/forumClasses/SearchBar.php
+++ WebContent/pageSnippets/forum/forumClasses/SearchBar.php
@@ -2,64 +2,37 @@
 
 class SearchBar
 {
-    public function __construct()
-    {
-
-    }
+    // SearchBar is the same object every single time. No need to assign instance variables.
+    // Only need to define the __toString() method so it can be echoed out easily.
+    public function __construct() { }
 
     public function __toString()
     {
         return
         '<nav class="navbar navbar-default center" role="navigation" style="margin-left: 10%; margin-right: 10%">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#">Search Bar</a>
+                    <a class="navbar-brand" href="#">Search the Forum</a>
                 </div>
 
-                    <div class="col-sm-3 col-md-3 pull-right">
-                        <form class="navbar-form" role="search">
+                    <div class="col-sm-5 col-md-5 pull-right" style="margin-right: 0px; padding-right: 5px;">
+                        <form class="navbar-form" role="search" style="margin-right: 0px; padding-right: 1px;">
                             <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Search" name="q">
+                                <input type="text" class="form-control" placeholder="Search" name="searchQuery">
                                 <div class="input-group-btn">
                                     <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
                                 </div>
+                                <select name="searchFilter" class="form-control">
+                                    <option value="WholeForum">Whole Forum</option>
+                                    <option value="ThreadTitlesOnly">Thread Titles Only</option>
+                                    <option value="PostsOnly">Posts Only</option>
+                                    <option value="GroupMembersOnly">Threads Including Group Member</option>
+                                </select>
                             </div>
                         </form>
                     </div>
 
             </nav>';
-
-
-
-
-       /*'<div class="container">
-                <div class="row">
-                    <div class="navbar navbar-default" role="navigation">
-
-                        <div class="collapse navbar-collapse navbar-ex1-collapse">
-                            <div class="span6 center">
-                                <form class="navbar-form" role="search" method="get" id="search-form" name="search-form">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" placeholder="Enter Search Term" id="query" name="query" value="">
-                                            <div class="input-group-btn">
-                                        <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-search"></span></button>
-                                        </div>
-                                    </div>
-
-                                    <div class="nav navbar-nav">
-                                        <div class="input-group"><a href="#" class="btn btn-warning">Search</a></div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-		</div>';*/
-
-
     }
-
-
-
 }
 
 
