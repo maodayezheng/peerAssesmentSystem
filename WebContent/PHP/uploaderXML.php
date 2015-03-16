5be5884f7fffaddbc58487e75a358edbd490d2f0
jobarry <jobarry@tcd.ie>
2015-03-16 10:40:42 +0000

    upload txt or xml report to db + display + remove dynamic

--- WebContent/PHP/uploaderXML.php
+++ WebContent/PHP/uploaderXML.php
@@ -24,32 +24,37 @@ if(isset($_POST['upload']) && $_FILES['userfile']['size'] > 0)
 		
 		echo $fileD;
 		
+		$fileContents = file_get_contents('up/xmlLG.xml');
+		
+		//echo $fileContents;
+		
+		$xml = simplexml_load_string($fileContents); # creates a Simple XML object from a string
+		
+		print_r(strval($xml));
+// 		 function xml2array ( $xml, $out = array () )
+// {
+// 	echo "hi";
+//     foreach ( (array) $xml as $index => $node )
+//         $out[$index] = ( is_object ( $node ) ) ? xml2array ( $node ) : $node;
+// print_r($out);
+//     return $out;
+}
+
+		
 	};
 	
-// $myXMLData =
-// "<?xml version='1.0' encoding='UTF-8'?>
-// <report>
-// <title>LG</title>
-// <abstract>new LG</abstract>
-// <body>improved LG</body>
-// </report>";
 
-// $xml=simplexml_load_string($myXMLData) or die("Error: Cannot create object");
-//print_r($xml);
 	
-	session_start();
+ 	session_start(); 
 
-// 	$peerGroup = $_SESSION ['peergroup'];
-// 	$sql = "INSERT INTO freetextreprots (id,content) VALUES ('$peerGroup','$content')";
+  	$peerGroup = $_SESSION ['peergroup']; 	
+  	$sql = "INSERT INTO freetextreprots (id,content) VALUES ('$peerGroup','$content')"; 
 	
-// 	if ($conn->query ( $sql ) === true) {
-// 		// output data of each row
-// 		echo "Selection complete";
-// 		//header ( 'location: ../gradeReport.php' );
-// 	} else {
-// 		echo "Error:" . $sql . "<br>" . $conn->error;
-// 	}
+  	if ($conn->query ( $sql ) === true) { 
+		 
+  		echo "Selection complete"; 
+ 		header ( 'location: ../gradeReport.php' ); 
+  	} else { 		echo "Error:" . $sql . "<br>" . $conn->error; 
+	}
 	
-	
-// 	header("location: ../submitReport.php");
- }
+?>
