49d2e3079ec68f10138e20e2b6908f40494621a0
vab2048 <vab2048@gmail.com>
2015-03-16 05:37:48 +0000

    Search Bar now added to forum page and positioning finalised

--- WebContent/forum.php
+++ WebContent/forum.php
@@ -1,12 +1,11 @@
 <?php
 
-require_once ("PHP/init.php");
-include ("header.php");
-include ("navbar.php");
-require_once("pageSnippets/forum/forumClasses/SearchBar.php");
+    require_once ("PHP/init.php");
+    include ("header.php");
+    include ("navbar.php");
+    require_once("pageSnippets/forum/forumClasses/SearchBar.php");
 
-$searchBar = new SearchBar();
-echo $searchBar;
+    echo new SearchBar();
 
 ?>
 
