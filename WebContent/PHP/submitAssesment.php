fe892760579e34b1c6ec3ed17abe5ba9136238bd
maodaye.zheng@gmail.com <maodaye.zheng@gmail.com>
2015-03-17 18:55:23 +0000

    test the submit assesment

---
+++ WebContent/PHP/submitAssesment.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'DBConnection.php';
+$comment = $_POST['content-comment'];
+$comment1 = $_POST['style-comment'];
+$comment2 = $_POST['source-comment'];
+$assesment = intval($_POST['content-grade']);
+$assesment1 = intval($_POST['style-grade']);
+$assesment2 = intval($_POST['source-grade']);
+$id = intval($_POST['id']);
+
+$sql = "update assesments set comment = $comment , comment1 = $comment1, comment2 = $comment2
+		Content =$assesment, Sytle = $assesment1, Sources = $assesment2 where id = $id";
+
+$conn->query($sql);
+?>
