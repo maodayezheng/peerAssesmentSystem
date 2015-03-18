47a2832fbc9ac37b8940559178a8c8e41f1ace2b
jobarry <jobarry@tcd.ie>
2015-03-18 02:04:50 +0000

    submit assessment

--- WebContent/PHP/submitAssesment.php
+++ WebContent/PHP/submitAssesment.php
@@ -8,8 +8,13 @@ $assesment1 = intval($_POST['style-grade']);
 $assesment2 = intval($_POST['source-grade']);
 $id = intval($_POST['id']);
 
-$sql = "update assesments set comment = $comment , comment1 = $comment1, comment2 = $comment2
-		Content =$assesment, Sytle = $assesment1, Sources = $assesment2 where id = $id";
+//$sql = "update assesments set comment = $comment , comment1 = $comment1, comment2 = $comment2
+		//Content =$assesment, Sytle = $assesment1, Sources = $assesment2 where id = 1";
+
+$sql = "UPDATE `peersystem`.`assesments` SET `comment`='$comment', `comment1`='$comment1', `comment2`='$comment2', `Content`='$assesment', `Style`='$assesment1', `Sources`='$assesment2' WHERE `groupAssessed`='$id';
+";
 
 $conn->query($sql);
+
+header('location: ../gradeReport.php');
 ?>
